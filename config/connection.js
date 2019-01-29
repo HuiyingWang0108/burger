ff9746cb030f06128cf25b498bece6abcde3d890
Huiying0108 <whymissu1228@gmail.com>
2019-01-29 11:37:08 -0800

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@ var mysql = require('mysql');
 // var PORT=process.env.PORT||3306;
 
 var connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-03.cleardb.net',
-    user: 'b3dafee0e8fa6d',
+    // host: 'us-cdbr-iron-east-03.cleardb.net',
+    // user: 'b3dafee0e8fa6d',
+    // port: 3306,
+    // password: '72602ec9',
+    // database: 'heroku_2dbde5aa5eda53f'
+    host: 'if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'i0m8qkmmv0o9s03a',
     port: 3306,
-    password: '72602ec9',
-    database: 'heroku_2dbde5aa5eda53f'
+    password: 'ajsmg85jng1tbev8',
+    database: 'kfb95jamaiq6qbc5'
 });
 
 connection.connect(function(err) {
