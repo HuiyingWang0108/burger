b11aa31b502fd6aa106bfd514dbba9e1593bb218
Huiying0108 <whymissu1228@gmail.com>
2019-01-28 16:01:53 -0800

    first commit

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
-var PORT=process.env.PORT||3306;
+// var PORT=process.env.PORT||3306;
 
 var connection = mysql.createConnection({
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'b3dafee0e8fa6d',
-    port: PORT,
+    port: 3306,
     password: '72602ec9',
     database: 'heroku_2dbde5aa5eda53f'
 });
 
 connection.connect(function(err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
+      console.error("error connecting*******: " + err.stack);
       return;
     }
     console.log("connected as id " + connection.threadId);
